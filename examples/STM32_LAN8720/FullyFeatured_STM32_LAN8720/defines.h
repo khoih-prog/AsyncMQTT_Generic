3bfd7c8a3165d1fccb483abfe97f5104c0e99169
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-03-16 00:59:32 -0400

    v1.2.1 to support STM32 + LAN8720
    ### Releases v1.2.1
    
    1. Add support to many **STM32F4 and STM32F7 (without TLS/SSL)** using `LAN8720` Ethernet, such as F407xx, NUCLEO_F429ZI, DISCO_F746NG, NUCLEO_F746ZG, NUCLEO_F756ZG, etc.
    2. Add examples for `STM32_LAN8720`

---
+++ examples/STM32_LAN8720/FullyFeatured_STM32_LAN8720/defines.h
@@ -0,0 +1,115 @@
+/****************************************************************************************************************************
+  defines.h
+  
+  defines.h
+  AsyncMqttClient_Generic is a library for ESP32, ESP8266, Protenta_H7, STM32F7, etc. with current AsyncTCP support
+  
+  Based on and modified from :
+  
+  1) async-mqtt-client (https://github.com/marvinroger/async-mqtt-client)
+  
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncMqttClient_Generic
+ *****************************************************************************************************************************/
+
+#ifndef defines_h
+#define defines_h
+
+#if !( defined(ARDUINO_BLACK_F407VE) || defined(ARDUINO_BLACK_F407VG) || defined(ARDUINO_BLACK_F407ZE) || defined(ARDUINO_BLACK_F407ZG)  || \
+       defined(ARDUINO_BLUE_F407VE_Mini) || defined(ARDUINO_DIYMORE_F407VGT) || defined(ARDUINO_FK407M1) || defined(ARDUINO_NUCLEO_F429ZI) || \
+       defined(ARDUINO_DISCO_F746NG) || defined(ARDUINO_NUCLEO_F746ZG) || defined(ARDUINO_NUCLEO_F756ZG) || defined(ARDUINO_NUCLEO_H743ZI) )
+  //#error This code is designed to run on some STM32F407XX NUCLEO-F429ZI, STM32F746 and STM32F756 platform! Please check your Tools->Board setting.
+#endif
+
+// Use from 0 to 4. Higher number, more debugging messages and memory usage.
+#define _ASYNC_MQTT_LOGLEVEL_               1
+
+#define USING_LAN8720                       true
+
+#if defined(STM32F0)
+  #warning STM32F0 board selected
+  #define BOARD_TYPE  "STM32F0"
+#elif defined(STM32F1)
+  #warning STM32F1 board selected
+  #define BOARD_TYPE  "STM32F1"
+#elif defined(STM32F2)
+  #warning STM32F2 board selected
+  #define BOARD_TYPE  "STM32F2"
+#elif defined(STM32F3)
+  #warning STM32F3 board selected
+  #define BOARD_TYPE  "STM32F3"
+#elif defined(STM32F4)
+  #warning STM32F4 board selected
+  #define BOARD_TYPE  "STM32F4"
+#elif defined(STM32F7)
+  #warning STM32F7 board selected
+  #define BOARD_TYPE  "STM32F7"
+#elif defined(STM32L0)
+  #warning STM32L0 board selected
+  #define BOARD_TYPE  "STM32L0"
+#elif defined(STM32L1)
+  #warning STM32L1 board selected
+  #define BOARD_TYPE  "STM32L1"
+#elif defined(STM32L4)
+  #warning STM32L4 board selected
+  #define BOARD_TYPE  "STM32L4"
+#elif defined(STM32H7)
+  #warning STM32H7 board selected
+  #define BOARD_TYPE  "STM32H7"
+#elif defined(STM32G0)
+  #warning STM32G0 board selected
+  #define BOARD_TYPE  "STM32G0"
+#elif defined(STM32G4)
+  #warning STM32G4 board selected
+  #define BOARD_TYPE  "STM32G4"
+#elif defined(STM32WB)
+  #warning STM32WB board selected
+  #define BOARD_TYPE  "STM32WB"
+#elif defined(STM32MP1)
+  #warning STM32MP1 board selected
+  #define BOARD_TYPE  "STM32MP1"
+#else
+  #warning STM32 unknown board selected
+  #define BOARD_TYPE  "STM32 Unknown"
+#endif
+
+#ifndef BOARD_NAME
+  #define BOARD_NAME    BOARD_TYPE
+#endif
+
+#define SHIELD_TYPE     "LAN8720 Ethernet"
+
+#include <LwIP.h>
+#include <STM32Ethernet.h>
+//#include <AsyncWebServer_STM32.h>
+
+// Enter a MAC address and IP address for your controller below.
+#define NUMBER_OF_MAC      20
+
+byte mac[][NUMBER_OF_MAC] =
+{
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x01 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x02 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x03 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x04 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x05 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x06 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x07 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x08 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x09 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x0A },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x0B },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x0C },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x0D },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x0E },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x0F },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x10 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x11 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x12 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x13 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0x32, 0x14 },
+};
+
+// Select the IP address according to your local network
+IPAddress ip(192, 168, 2, 232);
+
+#endif    //defines_h
---
+++ examples/STM32_LAN8720/FullyFeatured_STM32_LAN8720/hal_conf_extra.h
@@ -0,0 +1,8 @@
+// Note: Must replace 
+// .arduino15/packages/STMicroelectronics/hardware/stm32/2.x.y/system/STM32F4xx/stm32f4xx_hal_conf_default.h
+// .arduino15/packages/STM32/hardware/stm32/1.x.y/system/STM32F4xx/stm32f4xx_hal_conf_default.h
+// as described in README.md
+
+#define HAL_ETH_MODULE_ENABLED
+
+#define LAN8742A_PHY_ADDRESS            0x01U
